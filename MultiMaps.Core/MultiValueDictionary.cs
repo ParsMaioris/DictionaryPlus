b72aeaf7c9ad0c3ca89a35663a7bcc6ead8cb86c
InnovateWithRK <rk.go@icloud.com>
2025-03-22 16:35:58 -0700

    Add constructors to MultiValueDictionary for customizable capacity

--- MultiMaps.Core/MultiValueDictionary.cs
+++ MultiMaps.Core/MultiValueDictionary.cs
@@ -4,4 +4,14 @@ public class MultiValueDictionary<TKey, TValue>
 {
     private const int DefaultCapacity = 64;
     private const float LoadFactorThreshold = 0.75f;
+
+    private Bucket<TKey, TValue>[] _buckets;
+    private int _count;
+
+    public MultiValueDictionary(int capacity)
+    {
+        _buckets = new Bucket<TKey, TValue>[capacity];
+    }
+
+    public MultiValueDictionary() : this(DefaultCapacity) { }
 }
