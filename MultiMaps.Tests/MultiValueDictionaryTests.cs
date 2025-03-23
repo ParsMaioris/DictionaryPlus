62b2eddc7abae6747141a59454c9a60b088ef605
InnovateWithRK <rk.go@icloud.com>
2025-03-23 14:21:58 -0700

    Add unit test for Add and GetValues methods in MultiValueDictionary

---
+++ MultiMaps.Tests/MultiValueDictionaryTests.cs
@@ -0,0 +1,21 @@
+using MultiMaps.Core;
+
+namespace MultiMaps.Tests;
+
+[TestClass]
+public class MultiValueDictionaryTests
+{
+    [TestMethod]
+    public void AddAndGetValuesTest()
+    {
+        var dictionary = new MultiValueDictionary<string, int>();
+        dictionary.Add("fruits", 1);
+        dictionary.Add("fruits", 2);
+        dictionary.Add("fruits", 3);
+
+        var values = dictionary.GetValues("fruits");
+
+        Assert.AreEqual(3, values.Count);
+        CollectionAssert.AreEquivalent(new[] { 1, 2, 3 }, values.ToArray());
+    }
+}
