f81c7f3b6c44151449d8bc168239495b6f517d15
InnovateWithRK <rk.go@icloud.com>
2025-03-24 16:02:21 -0700

    Refactor KeyValueIterator for improved readability and maintainability

--- MultiMaps.Core/KeyValueIterator.cs
+++ MultiMaps.Core/KeyValueIterator.cs
@@ -6,85 +6,60 @@ internal class KeyValueIterator<TKey, TValue> : IEnumerator<KeyValuePair<TKey, T
 {
     private readonly MultiMap<TKey, TValue> _multiMap;
     private readonly object _syncRoot;
-    private int _expectedVersion;
-    private int _bucketIndex;
+    private readonly int _expectedVersion;
+
+    private int _currentBucketIndex;
     private Entry<TKey, TValue>? _currentEntry;
     private int _valueIndex;
-    private KeyValuePair<TKey, TValue>? _currentPair;
+    private KeyValuePair<TKey, TValue>? _currentKeyValue;
 
-    public KeyValueIterator(
-        MultiMap<TKey, TValue> dictionary,
-        object syncRoot)
+    public KeyValueIterator(MultiMap<TKey, TValue> multiMap, object syncRoot)
     {
-        _multiMap = dictionary;
+        _multiMap = multiMap;
         _syncRoot = syncRoot;
-        _expectedVersion = dictionary.Version;
-        _bucketIndex = -1;
+        _expectedVersion = multiMap.Version;
+
+        _currentBucketIndex = -1;
         _currentEntry = null;
         _valueIndex = -1;
-        _currentPair = null;
+        _currentKeyValue = null;
     }
 
-    public KeyValuePair<TKey, TValue> Current()
+    public KeyValuePair<TKey, TValue> Current
     {
-        if (_currentPair == null)
-            throw new InvalidOperationException("Enumeration not started or has ended.");
+        get
+        {
+            if (_currentKeyValue == null)
+                throw new InvalidOperationException("Enumeration not started or has ended.");
 
-        return _currentPair.Value;
+            return _currentKeyValue.Value;
+        }
     }
 
-    KeyValuePair<TKey, TValue> IEnumerator<KeyValuePair<TKey, TValue>>.Current => Current();
-
-    object IEnumerator.Current => Current();
+    object IEnumerator.Current => Current;
 
     public bool MoveNext()
     {
         lock (_syncRoot)
         {
-            if (_expectedVersion != _multiMap.Version)
-                throw new InvalidOperationException("Collection was modified during iteration.");
+            ValidateUnmodifiedCollection();
 
-            if (_currentEntry != null && _valueIndex < _currentEntry.Values.Count - 1)
-            {
-                _valueIndex++;
-                _currentPair = new KeyValuePair<TKey, TValue>(
-                    _currentEntry.Key,
-                    _currentEntry.Values[_valueIndex]);
+            if (TryMoveNextValueInCurrentEntry())
                 return true;
-            }
 
-            if (_currentEntry != null && _currentEntry.Next != null)
+            if (TryMoveToNextEntryInChain())
+                return true;
+
+            while (AdvanceToNextBucket())
             {
-                _currentEntry = _currentEntry.Next;
-                _valueIndex = 0;
-                if (_currentEntry.Values.Count > 0)
+                if (_currentEntry != null && _currentEntry.Values.Count > 0)
                 {
-                    _currentPair = new KeyValuePair<TKey, TValue>(
-                        _currentEntry.Key,
-                        _currentEntry.Values[_valueIndex]);
+                    _currentKeyValue = CreateKeyValue(_currentEntry, 0);
                     return true;
                 }
             }
 
-            while (++_bucketIndex < _multiMap.Buckets.Length)
-            {
-                var bucket = _multiMap.Buckets[_bucketIndex];
-                if (bucket == null || bucket.Head == null)
-                    continue;
-
-                _currentEntry = bucket.Head;
-                _valueIndex = 0;
-
-                if (_currentEntry.Values.Count > 0)
-                {
-                    _currentPair = new KeyValuePair<TKey, TValue>(
-                        _currentEntry.Key,
-                        _currentEntry.Values[_valueIndex]);
-                    return true;
-                }
-            }
-
-            _currentPair = null;
+            _currentKeyValue = null;
             return false;
         }
     }
@@ -93,30 +68,34 @@ internal class KeyValueIterator<TKey, TValue> : IEnumerator<KeyValuePair<TKey, T
     {
         lock (_syncRoot)
         {
-            if (_expectedVersion != _multiMap.Version)
-                throw new InvalidOperationException("Collection was modified during iteration.");
-
-            _bucketIndex = -1;
-            _currentEntry = null;
-            _valueIndex = -1;
-            _currentPair = null;
+            ValidateUnmodifiedCollection();
+            ResetIterationState();
         }
     }
 
     public void Dispose()
     {
+        // No resources to dispose
     }
 
     private void ValidateUnmodifiedCollection()
     {
         if (_expectedVersion != _multiMap.Version)
-        {
-            throw new InvalidOperationException(
-                "Collection was modified during iteration.");
-        }
+            throw new InvalidOperationException("Collection was modified during iteration.");
     }
 
-    private bool TryMoveNextEntryInChain()
+    private bool TryMoveNextValueInCurrentEntry()
+    {
+        if (_currentEntry != null && _valueIndex < _currentEntry.Values.Count - 1)
+        {
+            _valueIndex++;
+            _currentKeyValue = CreateKeyValue(_currentEntry, _valueIndex);
+            return true;
+        }
+        return false;
+    }
+
+    private bool TryMoveToNextEntryInChain()
     {
         if (_currentEntry != null && _currentEntry.Next != null)
         {
@@ -125,11 +104,40 @@ internal class KeyValueIterator<TKey, TValue> : IEnumerator<KeyValuePair<TKey, T
 
             if (_currentEntry.Values.Count > 0)
             {
-                //_currentEntry = CreateKeyValue(_currentEntry, _valueIndex);
+                _currentKeyValue = CreateKeyValue(_currentEntry, _valueIndex);
                 return true;
             }
         }
-
         return false;
+    }
+
+    private bool AdvanceToNextBucket()
+    {
+        if (++_currentBucketIndex >= _multiMap.Buckets.Length)
+            return false;
+
+        var bucket = _multiMap.Buckets[_currentBucketIndex];
+        if (bucket == null || bucket.Head == null)
+            return true;
+
+        _currentEntry = bucket.Head;
+        _valueIndex = 0;
+        return true;
+    }
+
+    private KeyValuePair<TKey, TValue> CreateKeyValue(Entry<TKey, TValue> entry, int valueIndex)
+    {
+        return new KeyValuePair<TKey, TValue>(
+            entry.Key,
+            entry.Values[valueIndex]
+        );
+    }
+
+    private void ResetIterationState()
+    {
+        _currentBucketIndex = -1;
+        _currentEntry = null;
+        _valueIndex = -1;
+        _currentKeyValue = null;
     }
 }
